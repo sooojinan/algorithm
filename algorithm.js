3f2be017bb0b094fc6573ba4c32d17ca4f7a5f4a
sujinan <sj.an@kukka.kr>
2023-02-14 10:36:21 +0900

    [algorithm] 69. Sqrt(x)

--- algorithm.js
+++ algorithm.js
@@ -1,12 +1,9 @@
 /**
- * @param {string} a
- * @param {string} b
- * @return {string}
+ * @param {number} x
+ * @return {number}
  */
- var addBinary = function(a, b) {
-    const aBigInt = `0b${a}`;
-    const bBigInt = `0b${b}`;
-    return (BigInt(aBigInt) + BigInt(bBigInt)).toString(2)
+ var mySqrt = function(x) {
+    return Math.floor(Math.sqrt(x))
 };
-addBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101","110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011")
-// addBinary("1010",'1011')
+
+console.log('mySqrt(4)',mySqrt(8))
